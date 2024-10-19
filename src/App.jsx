f5f649e5914e90b1325a0645dba759206e7d63ff
Suraj Bhatt <surajbhatt54321@gmail.com>
2024-10-19 22:04:05 +0530

    Add basic page components with placeholder headings

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
+// pages
+import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
+import ArticlesListPage from "./pages/ArticlesListPage";
+import ArticlePage from "./pages/ArticlePage";
+
+// css
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <h1>Blogify</h1>
-      <div id="page-body">Welcome to Blogify!</div>
+      <div id="page-body">
+        <HomePage />
+        <AboutPage />
+        <ArticlesListPage />
+        <ArticlePage />
+      </div>
     </div>
   );
 }
 
 export default App;
+// creating your blog pages
---
+++ src/pages/ArticlesListPage.jsx
@@ -0,0 +1,5 @@
+const ArticlesListPage = () => {
+  return <h1>Articles List Page</h1>;
+};
+
+export default ArticlesListPage;
