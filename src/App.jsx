322f04fb3b635aa033dc18a7f68ecc97da92faf1
Suraj Bhatt <surajbhatt54321@gmail.com>
2024-10-19 22:11:32 +0530

    setup routes for pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,6 @@
+// npm modules
+import { Route, Routes } from "react-router-dom";
+
 // pages
 import HomePage from "./pages/HomePage";
 import AboutPage from "./pages/AboutPage";
@@ -12,10 +15,12 @@ function App() {
     <div className="App">
       <h1>Blogify</h1>
       <div id="page-body">
-        <HomePage />
-        <AboutPage />
-        <ArticlesListPage />
-        <ArticlePage />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/articles" element={<ArticlesListPage />} />
+          <Route path="/articles/:articleId" element={<ArticlePage />} />
+        </Routes>
       </div>
     </div>
   );
