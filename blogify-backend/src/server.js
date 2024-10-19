062d99f48371422bc70fb9cde750ff468b0295b5
Suraj Bhatt <surajbhatt54321@gmail.com>
2024-10-20 00:45:06 +0530

    add upvote fuctionality with temp fake db

--- blogify-backend/src/server.js
+++ blogify-backend/src/server.js
@@ -1,11 +1,25 @@
 import express from "express";
 
+// fake db that will make development much easier,later will replace it with mongodb
+const articlesInfo = [
+  { name: "learn-react", upvotes: 0 },
+  { name: "learn-node", upvotes: 0 },
+  { name: "mongodb", upvotes: 0 },
+];
+
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("response");
+app.use(express.json());
+
+app.put("/api/articles/:name/upvote", (req, res) => {
+  const { name } = req.params;
+  const article = articlesInfo.find((article) => article.name === name);
+  if (article) {
+    article.upvotes += 1;
+    res.send(`The ${name} article has ${article.upvotes} upvotes.`);
+  }
 });
 
-app.listen(3000, (req, res) => {
-  console.log("server listening on port 8000");
+app.listen(3000, () => {
+  console.log("server listening on port 3000");
 });
