c9b7d28a865da929b59978fc3cc15248e72817bf
Suraj Bhatt <surajbhatt54321@gmail.com>
2024-10-20 01:03:34 +0530

    add comments functionality

--- blogify-backend/src/server.js
+++ blogify-backend/src/server.js
@@ -2,9 +2,9 @@ import express from "express";
 
 // fake db that will make development much easier,later will replace it with mongodb
 const articlesInfo = [
-  { name: "learn-react", upvotes: 0 },
-  { name: "learn-node", upvotes: 0 },
-  { name: "mongodb", upvotes: 0 },
+  { name: "learn-react", upvotes: 0, comments: [] },
+  { name: "learn-node", upvotes: 0, comments: [] },
+  { name: "mongodb", upvotes: 0, comments: [] },
 ];
 
 const app = express();
@@ -17,6 +17,20 @@ app.put("/api/articles/:name/upvote", (req, res) => {
   if (article) {
     article.upvotes += 1;
     res.send(`The ${name} article has ${article.upvotes} upvotes.`);
+  } else {
+    res.send("The article doesn't exist");
+  }
+});
+
+app.post("/api/articles/:name/comments", (req, res) => {
+  const { name } = req.params;
+  const { postedBy, text } = req.body;
+  const article = articlesInfo.find((article) => article.name === name);
+  if (article) {
+    article.comments.push({ postedBy, text });
+    res.send(article.comments);
+  } else {
+    res.send("The article doesn't exist");
   }
 });
 
