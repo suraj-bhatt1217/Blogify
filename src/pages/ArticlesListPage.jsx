08e45035666c010e5d5627dd1dff237a813e0b6c
Suraj Bhatt <surajbhatt54321@gmail.com>
2024-10-19 23:30:17 +0530

    add articleslist component and refactor articleslistpage to use it

--- src/pages/ArticlesListPage.jsx
+++ src/pages/ArticlesListPage.jsx
@@ -2,20 +2,13 @@ import { Link } from "react-router-dom";
 
 import articles from "../data/article-content";
 
+import ArticlesList from "../components/ArticlesList";
+
 const ArticlesListPage = () => {
   return (
     <>
       <h1>Articles</h1>
-      {articles.map((article) => (
-        <Link
-          key={article.name}
-          className="article-list-item"
-          to={`/articles/${article.name}`}
-        >
-          <h3>{article.title}</h3>
-          <p>{article.content[0].substring(0, 150)}...</p>
-        </Link>
-      ))}
+      <ArticlesList articles={articles} />
     </>
   );
 };
