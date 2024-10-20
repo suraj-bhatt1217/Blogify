83a4680ebc83ab8bab55a85077b09304fe781e4f
Suraj Bhatt <surajbhatt54321@gmail.com>
2024-10-20 15:02:38 +0530

    implement create account functionality

--- blogify-fe/src/pages/CreateAccountPage.jsx
+++ blogify-fe/src/pages/CreateAccountPage.jsx
@@ -1,5 +1,62 @@
+import { useState } from "react";
+
+import { Link, useNavigate } from "react-router-dom";
+
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+
 const CreateAccountPage = () => {
-  return <h1>Create Account</h1>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const navigate = useNavigate();
+
+  const createAccount = async () => {
+    try {
+      if (password !== confirmPassword) {
+        setError("Password and confirm password don't match");
+        return;
+      }
+      await createUserWithEmailAndPassword(getAuth(), email, password);
+      navigate("/articles");
+    } catch (e) {
+      setError(e.message);
+    }
+  };
+
+  return (
+    <>
+      <h1>Create Account</h1>
+      {error && <p className="error"> {error}</p>}
+      <input
+        type="email"
+        placeholder="Your email address"
+        value={email}
+        onChange={(e) => {
+          setEmail(e.target.value);
+        }}
+      />
+      <input
+        type="password"
+        value={password}
+        onChange={(e) => {
+          setPassword(e.target.value);
+        }}
+        placeholder="your password"
+      />
+      <input
+        type="password"
+        value={confirmPassword}
+        onChange={(e) => {
+          setConfirmPassword(e.target.value);
+        }}
+        placeholder="confirm password"
+      />
+      <button onClick={createAccount}>Create Account</button>
+      <Link to="/login">Already have an accoutn? Log in here</Link>
+    </>
+  );
 };
 
 export default CreateAccountPage;
