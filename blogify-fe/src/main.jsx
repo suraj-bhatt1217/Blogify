3f66de8c9b8b87202dcdf0b2091c7c346febe637
Suraj Bhatt <surajbhatt54321@gmail.com>
2024-10-20 14:49:30 +0530

    utilize useUser hook to render articlepage conditionaly

--- blogify-fe/src/main.jsx
+++ blogify-fe/src/main.jsx
@@ -4,6 +4,21 @@ import App from "./App.jsx";
 import { BrowserRouter as Router } from "react-router-dom";
 import "./index.css";
 
+import { initializeApp } from "firebase/app";
+
+// Firebase configuration using environment variables
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Router>
--- blogify-fe/src/pages/ArticlePage.jsx
+++ blogify-fe/src/pages/ArticlePage.jsx
@@ -10,12 +10,16 @@ import NotFoundPage from "./NotFoundPage";
 import CommentsList from "../components/CommentsList";
 import AddCommentForm from "../components/AddCommentForm";
 
+//hooks
+import useUser from "../hooks/useUser";
+
 const ArticlePage = () => {
   const [articleInfo, setArticleInfo] = useState({
     upvotes: 0,
     comments: [],
   });
   const { articleId } = useParams();
+  const { user, isLoading } = useUser();
 
   useEffect(() => {
     const loadArticleInfo = async () => {
@@ -51,15 +55,28 @@ const ArticlePage = () => {
   return (
     <>
       <h1>{article.title}</h1>
+
       <div className="upvotes-section">
-        <button onClick={addUpvote}>Upvote</button>
+        {user ? (
+          <button onClick={addUpvote}>Upvote</button>
+        ) : (
+          <button>Log in to upvote</button>
+        )}
         <p>This article has {articleInfo.upvotes} upvotes.</p>
       </div>
 
       {article.content.map((para, idx) => (
         <p key={idx}>{para}</p>
       ))}
-      <AddCommentForm setArticleInfo={setArticleInfo} articleName={articleId} />
+      {user ? (
+        <AddCommentForm
+          setArticleInfo={setArticleInfo}
+          articleName={articleId}
+        />
+      ) : (
+        <button>Log in to add comment</button>
+      )}
+
       <CommentsList comments={articleInfo.comments} />
     </>
   );
